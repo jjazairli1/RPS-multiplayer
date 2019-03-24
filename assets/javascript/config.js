e8420de5ff5e956f028db2dd1a471f6402a2a119
Jamal Jazairli <jjazairli1@gmail.com>
2019-03-24 17:13:47 -0500

    first commit

---
+++ assets/javascript/config.js
@@ -0,0 +1,13 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyBDD50BUNjLYhTHnrkUwEP6t9vQZ6rXS0Y",
+    authDomain: "rps-game-9e4ea.firebaseapp.com",
+    databaseURL: "https://rps-game-9e4ea.firebaseio.com",
+    projectId: "rps-game-9e4ea",
+    storageBucket: "rps-game-9e4ea.appspot.com",
+    messagingSenderId: "762086721675"
+  };
+  firebase.initializeApp(config);
+
+
+
